20853da8d5b1a84699f80dc9c70a893cd9712583
Kalli Rüis <kalli.ryis@concise.ee>
2021-12-07 14:55:05 +0200

    Add slider

--- my-app/components/Christmas.js
+++ my-app/components/Christmas.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import { Image } from "react-bootstrap";
+import SimpleImageSlider from "react-simple-image-slider";
+
+const images = [
+    { url: "../images/christmasnyan.jpg" },
+    { url: "../images/1.png" },
+    { url: "../images/2.png" },
+];
+
+const imagesWithUrl = [
+    { url: "https://wallpapercave.com/wp/wp688796.jpg" },
+    { url: "https://images.unsplash.com/photo-1606509779095-de8fd5187049?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hyaXN0bWFzJTIwY2F0fGVufDB8fDB8fA%3D%3D&w=1000&q=80" },
+    { url: "https://nypost.com/wp-content/uploads/sites/2/2019/12/christmas-cat-costume.jpg?quality=80&strip=all" },
+];
 
 export const Christmas = ({christmas}) => {
     return(
         <div className="container">
-            <Image src={'https://i.ytimg.com/vi/2cOUyWxVb5E/hqdefault.jpg'}/>
+            <SimpleImageSlider
+                width={1000}
+                height={1000}
+                images={imagesWithUrl}
+                showNavs={false}
+                showBullets={false}
+                autoPlay={true}
+                loop={true}
+            />
         </div>
     )
 }

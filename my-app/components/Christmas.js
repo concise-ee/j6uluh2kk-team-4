89cb5e91133f84576a67542986d3681ef7bd3bec
Caspar Romot <caspar.romot@concise.ee>
2021-12-07 16:45:04 +0200

    fix

--- my-app/components/Christmas.js
+++ my-app/components/Christmas.js
@@ -25,7 +25,7 @@ export const Christmas = ({christmas}) => {
                 autoPlay={true}
                 loop={true}
             />
-            <iframe style={{visibility: "hidden"}} width="560" height="315" src="https://www.youtube.com/embed/pzK3_MA1Al4?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe allow="autoplay" style={{visibility: "hidden"}} width="560" height="315" src="https://www.youtube.com/embed/pzK3_MA1Al4?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     )
 }
